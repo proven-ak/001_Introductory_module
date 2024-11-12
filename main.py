28614272c143d31f32cca35ec5f690a1b936585c
proven_ak <proven@ya.ru>
2024-11-12 23:37:54 +0300

    Итог

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-"""
+
 # 1st program
 print((9**0.5)*5)
 
@@ -9,6 +9,6 @@ print(9.99 > 9.98 and 1000 != 1000.1)
 print(2 * 2 + 2)
 print(2 * (2 + 2))
 print((2 * 2 + 2) == (2 * (2 + 2)))
-"""
+
 # 4th program
 print(int(float("123.456") * 10 % 10))
