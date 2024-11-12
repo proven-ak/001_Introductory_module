d30a3e0eeace4ba8d92bef78fbcbecfa380f3fd0
proven_ak <proven@ya.ru>
2024-11-12 23:27:45 +0300

    Многострочный код

---
+++ main.py
@@ -0,0 +1,18 @@
+"""
+# 1st program
+print((9**0.5)*5)
+
+# 2nd program
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+# 3rd program
+print(2 * 2 + 2)
+print(2 * (2 + 2))
+print((2 * 2 + 2) == (2 * (2 + 2)))
+"""
+# 4th program
+string = "123.456"
+number = float(string) * 10
+remains = number % 10
+integer = int(remains)
+print(string, number, remains, integer)
