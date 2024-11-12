a62b12f4423c4e5f97c92309d84340f4369f7930
proven_ak <proven@ya.ru>
2024-11-12 23:32:29 +0300

    Код в одну строку

--- main.py
+++ main.py
@@ -11,8 +11,4 @@ print(2 * (2 + 2))
 print((2 * 2 + 2) == (2 * (2 + 2)))
 """
 # 4th program
-string = "123.456"
-number = float(string) * 10
-remains = number % 10
-integer = int(remains)
-print(string, number, remains, integer)
+print(int(float("123.456") * 10 % 10))
